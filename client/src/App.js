7cf890936c42de04e9eec276d38970a66b220dcc
yoyoekta <yoyoekta@gmail.com>
2023-07-13 10:02:48 +0530

    Routes updated

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import MyProducts from "./pages/MyProducts";
+import AddProduct from "./pages/AddProduct";
+import Product from "./pages/Product";
+import EditProduct from "./pages/EditProduct"
 
 function App() {
   return (
@@ -16,6 +20,10 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/myproducts" element={<MyProducts />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/myproducts/edit/:id" element={<EditProduct />} />
       </Routes>
     </BrowserRouter>
   );
