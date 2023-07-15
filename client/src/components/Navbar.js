74cb1ec61fac93992549594d57d67b51700b3b0e
yoyoekta <yoyoekta@gmail.com>
2023-07-15 20:10:26 +0530

    Navbar updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaUser } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const checkToken = () => {
+        if(localStorage.getItem('token')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     const primary = [
         {
@@ -37,9 +49,25 @@ const Navbar = () => {
                         { primary.map((item, index) => {
                             return (
                                 <div key={index} className="flex font-normal text-lg space-x-4">
-                                    <NavLink to={item.link} className={({isActive}) => isActive && "underline underline-offset-8"}>
-                                        {item.name}
-                                    </NavLink>
+                                    { (checkToken() && item.name === 'Login/Signup') ?
+                                        (<div className="relative border border-slate-500 rounded-full p-2">
+                                            <FaUser onClick={()=>setOpen(!open)} className='cursor-pointer text-xl'/>
+                                            { open && 
+                                                <div className='z-10 absolute -right-4 top-10 bg-white border-2 rounded-xl border-black'>
+                                                    <div className='flex flex-col p-2 m-2 space-y-1  '>
+                                                        <NavLink to='/myproducts' className='text-slate-500 hover:text-slate-700'>My&nbsp;Products</NavLink>
+                                                        <hr className='bg-slate-500 border-2 h-1'/>
+                                                        <NavLink to='/addproduct' className='text-slate-500 hover:text-slate-700 '>Add&nbsp;Product</NavLink>
+                                                        <hr className='bg-slate-500 border-2 h-1'/>
+                                                        <NavLink to='/logout' className='text-slate-500 hover:text-slate-700'>Logout</NavLink>
+                                                    </div>
+                                                </div>
+                                            }
+                                        </div>) :
+                                        (<NavLink to={item.link} className={({isActive}) => isActive && "underline underline-offset-8"}>
+                                            {item.name}
+                                        </NavLink>)
+                                    }
                                 </div>
                             )}
                         )}
