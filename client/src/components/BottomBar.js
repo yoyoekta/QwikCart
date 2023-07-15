be6eb059102e4484cfdcb320be8062b5222fe623
yoyoekta <yoyoekta@gmail.com>
2023-07-15 20:12:00 +0530

    Bottom Bar updated with links and styles

--- client/src/components/BottomBar.js
+++ client/src/components/BottomBar.js
@@ -1,33 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const BottomBar = () => {
   return (
     <nav className="min-w-screen bg-black text-white">
             <div className="mx-5">
-                <div className="relative flex items-center h-10">
-                <div className="flex justify-between items-center w-full">
-                    
-                    <div className="flex space-x-4 items-center">
-                        <div className="flex space-x-4">
-                            All Products
-                        </div>
-                        <div className="flex space-x-4">
-                            Electronics
-                        </div>
-                        <div className="flex space-x-4">
-                            Clothing
-                        </div>
-                        <div className="flex space-x-4">
-                            Household
-                        </div>
-                        <div className="flex space-x-4">
-                            Art &amp; Crafts
+                <div className="relative flex items-center h-12">
+                    <div className="flex justify-between items-center w-full">
+                        
+                        <div className="flex items-center">
+                            <NavLink to="/allproducts" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md">
+                                All Products
+                            </NavLink>
+                            <NavLink to="/products/electronics" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md">
+                                Electronics
+                            </NavLink>
+                            <NavLink to="/products/clothing" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md">
+                                Clothing
+                            </NavLink>
+                            <NavLink to="/products/household" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md">
+                                Household
+                            </NavLink>
+                            <NavLink to="/products/art & crafts" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md">
+                                Art &amp; Crafts
+                            </NavLink>
                         </div>
                     </div>
-                    <div className="flex-shrink-0 flex flex-row space-x-4">
-                        Search
-                    </div>
-                </div>
                 </div>
             </div>
         </nav>
