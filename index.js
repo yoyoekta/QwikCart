ba939f2598c22ce1db8146701b46113fc8451700
yoyoekta <yoyoekta@gmail.com>
2023-07-16 16:04:13 +0530

    Stripe for backend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const auth = require('./routes/auth')
 const admin = require('./routes/admin')
 const home = require('./routes/home')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 dotenv.config();
 
@@ -21,6 +22,50 @@ app.use(express.json({limit:"50mb"}));
 app.use("/api/auth", auth);
 app.use("/api/admin", admin);
 app.use("/api/home", home);
+
+
+// Stripe
+app.post("/api/checkout", async (req, res) => {
+    console.log(req.body);
+
+    try{
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            billing_address_collection: "auto",
+            shipping_options: [{shipping_rate : "shr_1NUBYoSApopPbzRopU4nOkcj"}],
+            mode: "payment",
+            line_items: req.body.items.map((item) => {
+                return {
+                    price_data: {
+                        currency: "inr",
+                        product_data: {
+                            name: item.itemName,
+                            images: [item.image],
+                        },
+                        unit_amount: item.price * 100,
+                    },
+                    adjustable_quantity: {
+                        enabled: true,
+                        minimum: 1,
+                    },
+                    quantity: item.quantity,
+                };
+            }),
+            success_url: `${process.env.CLIENT_URL}/success`,
+            cancel_url: `${process.env.CLIENT_URL}/cancel`,
+            
+        });
+        res.status(200).send({ id: session.id });
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send({
+            "success": false,
+            "message":"Payment Failed"
+        })
+    }
+})
+
 
 app.listen(PORT, async () => {
     console.log(`Server started at ${PORT}`);
